53a7550221ca2ced680918b7246e564db5f1233c
sarabkahlon <sarabjeet11062001@gmail.com>
2023-02-01 16:06:01 -0500

    your commit comment

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -2,9 +2,11 @@
 
 echo " `hostnamectl`"
 #this command is used to read and edit  the  system hostnmae related settings.
-echo "`hostname`"
+echo "FQDN: $(hostname)"
 #this command shows system hostname  information
+echo "Root Filesystem Status:"
 echo "`df -h /`"
 # this command generally shows how much free space is availible  in the file system.
+echo "IP addresses:"
 echo "`hostname -I`"
 #this command shows the  current  ip address used by the system
