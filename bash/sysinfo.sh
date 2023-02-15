b3c0230874cd31abb2ef1cd33ebc7ccbecf3c9a8
sarabkahlon <sarabjeet11062001@gmail.com>
2023-02-15 16:06:55 -0500

    lab2

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #The purpose of this script is to display some important identity information about a computer 
 
-C=$(df -h / |grep Avail -v |awk '{print $4}')
 
 A=$(lsb_release -d | awk '{print $2,$3,$4}')
 
 B=$(hostname -I | awk '{print $1}')
+
+C=$(df -h / |grep Avail -v |awk '{print $4}')
 
 
 cat << EOF
