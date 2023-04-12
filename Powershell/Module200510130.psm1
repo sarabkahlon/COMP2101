4346fb5cbea5877b255810aa87ca17669de481f6
sarabkahlon <sarabkahlon05@gmail.com>
2023-04-12 14:49:35 -0700

    module file

---
+++ Powershell/Module200510130.psm1
@@ -0,0 +1,15 @@
+function welcome {
+# Lab 2 COMP2101 welcome script for profile
+#
+write-output "Welcome to planet $env:computername Overlord $env:username"
+$now = get-date -format 'HH:MM tt on dddd'
+write-output "It is $now." 
+}
+
+function get-cpuinfo {
+get-ciminstance cim_processor | format-list "Manufacturer","Name","MaxClockSpeed","NumberOfCores","CurrentClockSpeed","Model"
+}
+
+function get-mydisks {
+get-PhysicalDisk | Select-Object "Manufacturer","Model","SerialNumber","FirmwareRevision","Size" | format-table
+}
